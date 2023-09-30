c0f988919cc904f7ce3f139b5c1d60b74d284883
Vivek Sharma <viveksharma1282003@gmail.com>
2023-09-30 11:01:43 +0530

    a

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.mjs'
 
 var swiper = new Swiper('.swiper', {
--- script2.js
+++ script2.js
@@ -8,3 +8,22 @@ function onClickMenu() {
     nav.classList.toggle("change");
     swip.classList.toggle("sw");
 }
+
+var count=0;
+function completed()
+{
+
+    var comp="completed";
+    var red="to be read";
+    if(count%2==0)
+    {
+        document.getElementsByClassName("read").innerHTML=comp;
+        document.getElementsByClassName("read").style.backgroundColor="white";
+    }
+    else{
+        
+        document.getElementsByClassName("read").innerHTML=red;
+        document.getElementsByClassName("read").style.backgroundColor="rgb(76, 148, 172)";
+    }
+    count++;
+}
